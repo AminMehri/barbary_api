5397fe8ceb6e261f06a5d829b92dca6a9ac120ce
Your Name <you@example.com>
2022-02-19 11:57:53 +0330

    add some actions

--- barbary_backend/barbary_api/admin.py
+++ barbary_backend/barbary_api/admin.py
@@ -22,6 +22,29 @@ def make_not_authenticate(modeladmin, request, queryset):
     make_not_authenticate.short_description = "Mark selected users as not authenticate"
 
 
+
+
+def make_see_notification(modeladmin, request, queryset):
+    rows_updated = queryset.update(isSeeLastNotif=True)
+    if rows_updated == 1:
+        message_bit = "1 story was"
+    else:
+        message_bit = "%s stories were" % rows_updated
+    modeladmin.message_user(request, "%s successfully marked as see notification." % message_bit)
+    make_see_notification.short_description = "Mark selected users as see notification"
+
+
+def make_dont_see_notification(modeladmin, request, queryset):
+    rows_updated = queryset.update(isSeeLastNotif=False)
+    if rows_updated == 1:
+        message_bit = "1 story was"
+    else:
+        message_bit = "%s stories were" % rows_updated
+    modeladmin.message_user(request, "%s successfully marked as dont see notification." % message_bit)
+    make_dont_see_notification.short_description = "Mark selected users as dont see notification"
+
+
+
 class BarAdmin(admin.ModelAdmin):
     list_display = ['beginning', 'destination', 'owner_bar', 'date', 'driver', 'isSpecial', 'isTopShow']
 
@@ -39,7 +62,7 @@ admin.site.register(Notification, NotificationAdmin)
 class AccountAdmin(admin.ModelAdmin):
 
     list_display = ['user', 'phone_number', 'isAuthenticated', 'name', 'position', 'isSeeLastNotif', 'first_time']
-    actions = [make_authenticate, make_not_authenticate]
+    actions = [make_authenticate, make_not_authenticate, make_see_notification, make_dont_see_notification]
 
 
 admin.site.register(Account, AccountAdmin)
